dd6bbfcac20c60ffb3f56bf7585cdd735cf95680
mohammadjewelrana <js.rana0326@gmail.com>
2026-07-20 12:13:01 +0600

    scroll to top components

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -2,10 +2,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
+import ScrollToTop from "../shared/SCrollToTop";
 
 const MainLayout = () => {
   return (
     <div>
+      <ScrollToTop/>
       <Navbar />
       <Outlet />
       <Footer />
